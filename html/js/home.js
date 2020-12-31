e3cbfa92302f89f05b7593c32e1bb6b4169112a8
Islam Ramdan <Islam131871@bue.edu.eg>
2020-12-31 08:53:22 +0200

    pagination, next, prev

--- html/js/home.js
+++ html/js/home.js
@@ -17,12 +17,65 @@ onload = function () {
   ];
   var index = 1;
   var slide = document.getElementById("slideImage");
+  
+  // -------- Function to get the next image
+
+  $("#next").click(function () { 
+    goToSlide(index + 1);
+  });
+
+  //------------ function to get the previus Image
+
+  $("#prev").click(function () { 
+    goToSlide(index - 1);
+  });
+
+  //-----------function to display slideshow
+
   setInterval(function () {
-    if (index < images.length - 1) {
-      index++;
-    } else {
-      index = 0;
-    }
-    slide.setAttribute("src", images[index]);
+    goToSlide(index + 1);
   }, 3000);
+
+
+  // ------- creation of pagination
+
+  var paginationElements = document.createElement('ul');
+  
+  paginationElements.setAttribute("id", "pagination-ul");
+
+  paginationElements.style.textAlign = "center";
+  // create list of lis based on images.length
+
+  for (var i = 1; i <= images.length; i++){
+    var liItem = document.createElement("li");
+    liItem.setAttribute("data-index", i);   // set data index for each element
+    liItem.setAttribute("class", "dot");   // add class active to li 
+    paginationElements.appendChild(liItem); // add li to ul
+  }
+
+  document.getElementById("pagination").appendChild(paginationElements);  // append ul to document
+  paginationElements.children[index].classList.add('active');            // set the current index to be active
+  
+  
+  for (var i = 0; i < paginationElements.children.length; i++){
+    paginationElements.children[i].onclick = function () {
+      index = parseInt(this.getAttribute('data-index')) - 1;
+      goToSlide(index);
+    }
+  }
+
+function goToSlide(num){
+  index = (num + images.length) % images.length;  // set index
+  slide.setAttribute("src", images[index]);
+  checkPagination();
+  }
+  
+  // function to set the current li element only to active
+  function checkPagination() {
+    for (var x = 0; x < paginationElements.children.length; x++)
+      paginationElements.children[x].classList.remove('active');
+      paginationElements.children[index].classList.add('active');
+  }
+
+
 };
