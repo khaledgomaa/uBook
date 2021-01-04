c2fc052fe92d0a71899a52885b6a2c0abb69b4c1
khaledgomaa <khaledgomaa670@gmail.com>
2021-01-04 23:27:42 +0200

    Updated form validation for contact us page

--- js/contactUs/contact.js
+++ js/contactUs/contact.js
@@ -4,7 +4,35 @@ onload = function () {
 };
 
 $(".submitForm").click(function () {
-  $(".thankMessage").show();
+  if (
+    $("#name").val() !== "" &&
+    $("#email").val() !== "" &&
+    $("#subject").val() !== "" &&
+    $("#message").val() !== ""
+  ) {
+    var emailRegex = /^(\w|[-]|[.])+@[a-z]+[\.][a-z]{2,8}$/;
+    if ($("#email").val().match(emailRegex)) {
+      $(".thankMessage").show();
+      $(".Islam").hide();
+      setTimeout(function () {
+        resetForm();
+      }, 3000);
+    } else {
+      $(".thankMessage").hide();
+      $(".Islam").show();
+    }
+  } else {
+    $(".thankMessage").hide();
+  }
 });
+
+function resetForm() {
+  $("#name").val("");
+  $("#email").val("");
+  $("#subject").val("");
+  $("#message").val("");
+  $(".thankMessage").hide();
+  $(".Islam").hide();
+}
 
 function submitInfo() {}
