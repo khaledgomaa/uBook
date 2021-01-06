d6d20d6d40a85eec4b9ecbacb09851845788f99b
khaledgomaa <khaledgomaa670@gmail.com>
2021-01-06 17:23:16 +0200

    Added singin and signup

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,56 +1,51 @@
 /*validation*/
 $(function () {
-    $('.login-form').validate({ 
+  $(".login-form").validate({
     rules: {
-        email: {
-            required: true,
-            email:true
-
-        },
-        password: {
-            required: true,
-        }
+      email: {
+        required: true,
+        email: true,
+      },
+      password: {
+        required: true,
+      },
     },
-    submitHandler: function(form){ 
-        if(login())
-            form.submit();
-      }
-    });
+    submitHandler: function (form) {
+      if (login()) form.submit();
+    },
+  });
 });
 /************************************** */
-(function(){
-    
-    if(cookie.getCookie(cookieNamecurrentUser) != "Not Found"){
-        location.replace("home.html")
-    }
-})()
+// (function(){
 
+//     if(cookie.getCookie("useremail") != "Not Found"){
+//         location.replace("home.html")
+//     }
+// })()
 
-let login = function(){
-    let email, password, remember_me;
-    email = document.getElementById("email").value;
-    password = document.getElementById("password").value;
-    remember_me = document.getElementById("remember_me").checked;
+let login = function () {
+  let email, password, remember_me;
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  remember_me = document.getElementById("remember_me").checked;
 
-    let userObj = JSON.parse(localStorage.getItem(localStorageForAccounts));
-    
-    if (userObj){
-        for(var i=0; i<userObj.length; i++){
-            if(userObj[i].email === email){
-                if(userObj[i].password === password){
-                    if(remember_me){
-                        let date = new Date();
-                        date.setMonth(date.getMonth() + 1);
-                        cookie.setCookie(cookieNamecurrentUser, email, date);                        
-                    }
-                    else   cookie.setCookie(cookieNamecurrentUser, email);                        
-                    return true;
-                }
-            }
+  let userObj = JSON.parse(localStorage.getItem(localStorageForAccounts));
+
+  if (userObj) {
+    for (var i = 0; i < userObj.length; i++) {
+      if (userObj[i].email === email) {
+        if (userObj[i].password === password) {
+          if (remember_me) {
+            let date = new Date();
+            date.setMonth(date.getMonth() + 1);
+            cookie.setCookie("useremail", email, date);
+          } else cookie.setCookie("useremail", email);
+          return true;
         }
+      }
     }
+  }
 
-    alert("wrong email or password")
-    return false;
-
-}
+  alert("wrong email or password");
+  return false;
+};
