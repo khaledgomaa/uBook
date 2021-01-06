855d67dc212bb7aac1580062cc030d165683b98d
mohamed12samy <mohamedsamyabbas99@gmail.com>
2021-01-06 16:05:03 +0200

    user local storage and cookie

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,2 +1,56 @@
-$("#signinHeader").load("./navbar.html");
-$("#signinFooter").load("./footer.html");
+/*validation*/
+$(function () {
+    $('.login-form').validate({ 
+    rules: {
+        email: {
+            required: true,
+            email:true
+
+        },
+        password: {
+            required: true,
+        }
+    },
+    submitHandler: function(form){ 
+        if(login())
+            form.submit();
+      }
+    });
+});
+/************************************** */
+(function(){
+    
+    if(cookie.getCookie(cookieNamecurrentUser) != "Not Found"){
+        location.replace("home.html")
+    }
+})()
+
+
+let login = function(){
+    let email, password, remember_me;
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+    remember_me = document.getElementById("remember_me").checked;
+
+    let userObj = JSON.parse(localStorage.getItem(localStorageForAccounts));
+    
+    if (userObj){
+        for(var i=0; i<userObj.length; i++){
+            if(userObj[i].email === email){
+                if(userObj[i].password === password){
+                    if(remember_me){
+                        let date = new Date();
+                        date.setMonth(date.getMonth() + 1);
+                        cookie.setCookie(cookieNamecurrentUser, email, date);                        
+                    }
+                    else   cookie.setCookie(cookieNamecurrentUser, email);                        
+                    return true;
+                }
+            }
+        }
+    }
+
+    alert("wrong email or password")
+    return false;
+
+}
---
+++ js/auth/signup.js
@@ -0,0 +1,71 @@
+/*validation*/
+$(function () {
+    $.validator.addMethod("passwordCheck", function(value) {
+        return /^[A-Za-z0-9=!\-@._*]*$/.test(value) && /[a-z]/.test(value) && /[0-9]/.test(value) && /[A-Z]/.test(value);
+    });
+$('.signup-form').validate({ 
+  rules: {
+      username: {
+          required: true,
+
+      },
+      email:{
+          required:true,
+      },
+      password: {
+          required: true,
+          minlength:8,
+          passwordCheck:true,
+      }
+  },
+  messages:{
+      password:{
+      passwordCheck:"Invalid password "
+      }
+    },
+    submitHandler: function(form) {
+        if(register())
+            form.submit();
+      }
+    });
+});
+/******************************************************************************************************************************* */
+(function(){
+    
+    if(cookie.getCookie(cookieNamecurrentUser) != "Not Found")
+        location.replace("home.html")
+})()
+let register = function(){
+    let email, username, password;
+    email = document.getElementById("email").value;
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    
+    let user = [{
+        'email': email,
+        'username': username,
+        'password': password
+    }];
+
+    let userObj = JSON.parse(localStorage.getItem(localStorageForAccounts));
+    var _new = true;
+    if (userObj){
+        for(var i=0; i<userObj.length; i++){
+            if(userObj[i].email === email){
+                alert("This email is already exist")
+                _new = false;
+                return false;
+            }
+        }
+        if(_new){
+            userObj = [...userObj, ...user]
+            localStorage.setItem(localStorageForAccounts,JSON.stringify(userObj))
+            cookie.setCookie(cookieNamecurrentUser, email);
+            return true;
+        }
+    }else {
+        localStorage.setItem(localStorageForAccounts,JSON.stringify(user));
+        cookie.setCookie(cookieNamecurrentUser, email);
+        return true;
+    }
+}
